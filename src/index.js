1bd52f14e5dfef69827d31d71cbc71c856380268
aaron isaac <aaron@aisaac.co>
2020-07-04 18:48:34 -0600

    Migrate old react application over.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import thunk from "redux-thunk";
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from "redux-devtools-extension";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from "./App";
+import Reducers from "./redux/reducers/Reducers";
+
+
+const store = createStore(
+    Reducers,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 serviceWorker.unregister();
+
